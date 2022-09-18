368a6907babaca943d3b79e541f007b2b92d0e0d
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-09-18 19:54:42 -0300

    exer 1

---
+++ callback - promise - async.await/exercícios aula2/exer.js
@@ -0,0 +1,15 @@
+/*
+​1) Em linguagens síncronas é comum a função sleep(tempo) assim,
+ pode-se por exemplo escrever algo no console, pausar o script por x tempo e escrever algo mais algo como:
+console.log('Olá!')
+sleep(2000)
+console.log('Depois de 2segs')
+simule o sleep de maneira correta
+ em Javascript (sem travar a thread principal) utilizando promises e uma função async/await
+*/
+
+const sleep = setTimeout(() => {
+	console.log("olá depois de ");
+});
+
+sleep(2000);
