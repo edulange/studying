ec8c9210ccb91cfc5fc4b11c2ca35e224c16f4d9
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-09-19 19:00:41 -0300

    ex 2 final

---
+++ callback - promise - async.await/exercícios aula2/exer2.js
@@ -0,0 +1,23 @@
+/*
+2) A função readdir disponível no módulo fs do Node,
+ permite ler um diretório, e retornar seu conteúdo como um vetor. 
+ Crie uma versão em Promise da função.
+
+Dica: a assinatura da função é: fs.readdir(caminho, (err, files) => {})
+*/
+const fs = require("fs");
+
+const readFile = (file) =>
+	new Promise((resolve, reject) => {
+		//dado uma promessa eu tenho duas opções, eu faço algo(resolvo) ou eu rejeito algo
+		fs.readdir(file, (err, contents) => {
+			//o famoso callback
+			if (err) {
+				//se deu erro! (se erro = true)
+				reject(err); // eu rejeito
+			} else {
+				//senão eu aceito
+				resolve(contents); // eu resolvo o conteudo
+			}
+		});
+	});
