63d4f27035e6b34ba2b090d206f1defcce2bc3df
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-06 17:27:39 -0300

    fixing

--- closures/closure.js
+++ closures/closure.js
@@ -1,5 +1,5 @@
-function func1() {
-    let contador = 0
+function func1(initial) {
+    let contador = initial
 
     return function() {
         contador++
@@ -7,5 +7,28 @@ function func1() {
     }
 }
 
-const func = func1()
+const func = func1(10)
+const funcB = func1(20)
 console.log(func())
+console.log(func())
+console.log(funcB(), funcB(), func())
+// ----------- assim continua o contador isolado separadamente
+
+function animal(name) {
+    let count = 0
+
+    return {
+        getCount: function() {
+            return count
+        },
+        increment: function() {
+            count++
+        },
+        getName: function() {
+            return name
+        }
+    }
+}
+const gato = animal('gato')
+gato.increment()
+console.log(gato.getCount(), gato.getName())
