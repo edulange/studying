72056c0a1f59712075183ec0b732a0fe5173a5a8
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-20 16:29:02 -0300

    finishing 16

--- curso-de-js-roger-melo-master/exercicio-16/app.js
+++ curso-de-js-roger-melo-master/exercicio-16/app.js
@@ -5,18 +5,19 @@
     'Clicou na div.' não seja exibida no console.
 */
 
-const div = document.querySelector('div')
-const elementsInsideDiv = Array.from(div.children)
+const div = document.querySelector("[data-div]"); //ok :)
 
-elementsInsideDiv.forEach(element => {
-  element.addEventListener('click', () => {
-    console.log('Clicou no filho da div.')
-  })
-})
+const elementsInsideDiv = Array.from(div.children); // transformei em array
+console.log(elementsInsideDiv);
 
-div.addEventListener('click', () => {
-  console.log('Clicou na div.')
-})
+elementsInsideDiv.forEach((element) => {
+	element.addEventListener("click", () => {
+		newh2.textContent += element.innerHTML + " "; // ex 3
+		console.log(`Clicou no ${element.tagName}, filho da div`); // ex
+	});
+});
+
+div.addEventListener("click", () => {});
 
 /*
   02
@@ -34,12 +35,19 @@ div.addEventListener('click', () => {
     filho da div, ao invés de ser exibida no console, seja inserida neste h2.
 */
 
+const newh2 = document.createElement("h2");
+document.body.insertBefore(newh2, div.nextSibling); // não existe .insertAfter, mas como eu falei pra inserir before do próximo sibling, é a posição certa, imagina assim: "1 passo pra frente" "1 passo pra trás"
+
 /*
   04
 
   - Faça com que quando o texto do h2 for copiado, a mensagem "Texto copiado!"  
     seja exibida no console.
 */
+
+newh2.addEventListener("copy", () => {
+	console.log("Texto copiado");
+});
 
 /*
   05
@@ -48,6 +56,11 @@ div.addEventListener('click', () => {
     o texto que ela tem por 
     "Eixo X: COORDENADA_EIXO_X | Eixo Y: COORDENADA_EIXO_Y".
 */
+const divegg = document.getElementsByClassName("egg")[0]; // pq ele cria um array, ai eu já quero pos 0 "elementSSSSS" (plural)
+
+divegg.addEventListener("mousemove", (event) => {
+	console.log(`Eixo X: ${event.clientX}, Eixo Y: ${event.clientY}`);
+});
 
 /*
   06
@@ -55,6 +68,12 @@ div.addEventListener('click', () => {
   - Modifique a cor do ovo para "lightgoldenrodyellow" quando o botão for 
     clicado.
 */
+
+const button = document.querySelector("[data-button]");
+
+button.addEventListener("click", () => {
+  divegg.style.backgroundColor  = 'lightgoldenrodyellow'
+});
 
 /*
   07
@@ -66,13 +85,19 @@ div.addEventListener('click', () => {
 */
 
 const people = [
-  { id: 1, name: 'Pedro Henrique', profession: 'Dentista' },
-  { id: 2, name: 'Fábio Alexandre', profession: 'Físico' },
-  { id: 3, name: 'Thiago Ferreira', profession: 'Veterinário' },
-  { id: 4, name: 'Marcelo Antonio', profession: 'Matemático' },
-  { id: 5, name: 'Camilla Midori', profession: 'Engenheira Civil' },
-  { id: 6, name: 'Gustavo D\'Aqui', profession: 'Gerente de Marketing' },
-  { id: 7, name: 'Ana Paula', profession: 'Front-end developer' },
-  { id: 8, name: 'Matheus Manucci', profession: 'Piloto' },
-  { id: 9, name: 'Hamilton Silva', profession: 'Advogado' }
-]
+	{ id: 1, name: "Pedro Henrique", profession: "Dentista" },
+	{ id: 2, name: "Fábio Alexandre", profession: "Físico" },
+	{ id: 3, name: "Thiago Ferreira", profession: "Veterinário" },
+	{ id: 4, name: "Marcelo Antonio", profession: "Matemático" },
+	{ id: 5, name: "Camilla Midori", profession: "Engenheira Civil" },
+	{ id: 6, name: "Gustavo D'Aqui", profession: "Gerente de Marketing" },
+	{ id: 7, name: "Ana Paula", profession: "Front-end developer" },
+	{ id: 8, name: "Matheus Manucci", profession: "Piloto" },
+	{ id: 9, name: "Hamilton Silva", profession: "Advogado" },
+];
+
+people.forEach(element => {
+  if (element.profession === "Front-end developer") {
+    console.log('existe 1')
+  }
+});
