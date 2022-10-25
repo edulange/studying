562dc44385dfb89b186c96d25fbad520f124e92a
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-25 15:58:47 -0300

    finished second formvalidation

--- FormValidation - ClientSide/formvalidationwhiletyping/script.js
+++ FormValidation - ClientSide/formvalidationwhiletyping/script.js
@@ -1,6 +1,40 @@
-const form = document.querySelector('#login-form')
+const form = document.querySelector("#login-form");
 
-let username = form.elements.namedItem("username")
-console.log(username)
-let password = form.elements.namedItem("password")
-console.log(password)
+let username = form.elements.namedItem("username");
+let password = form.elements.namedItem("password");
+
+const pass_reg = /[a-zA-Z\d]{6,}$/;
+// merda de regex
+
+username.addEventListener("input", validate);
+password.addEventListener("input", validate);
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	alert("SUBMITED");
+});
+
+function validate(e) {
+	let target = e.target;
+	if (target.name == "password") {
+		//validação password
+		if (pass_reg.test(target.value)) {
+			target.classList.add("valid");
+			target.classList.remove("invalid");
+		} else {
+			target.classList.add("invalid");
+			target.classList.remove("valid");
+		}
+	}
+	if (target.name == "username") {
+		//validação username
+		if (target.value.length >= 4) {
+			target.classList.add("valid");
+			target.classList.remove("invalid");
+		} else {
+			target.classList.add("invalid");
+			target.classList.remove("valid");
+		}
+	}
+}
