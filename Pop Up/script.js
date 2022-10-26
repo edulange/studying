e25b897f4743e0d54a845169eff6efa1d0012e18
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-26 14:32:48 -0300

    finishing popup xD

--- Pop Up/script.js
+++ Pop Up/script.js
@@ -1,14 +1,17 @@
 // a exibição do popup depende de um click no botão
-const button = document.querySelector('button')
-const popup = document.querySelector('.popup-wrapper')
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup-wrapper");
 
-button.addEventListener('click', () => {
-    popup.style.display = "block"
-})
+button.addEventListener("click", () => {
+	popup.style.display = "block";
+});
 
+popup.addEventListener("click", (event) => {
+	const classNameOfClickedElement = event.target.classList[0];
+    const classNames = ['popup-close', 'popup-wrapper', 'popup-link']
+    const shouldClosePopup = classNames.some(className => className === classNameOfClickedElement)
 
-popup.addEventListener('click', (e) => {
-    console.log(e.target)
-    //popup.style.display = 'none'
-})
-
+    if (shouldClosePopup) {
+		popup.style.display = "none";
+	}
+});
