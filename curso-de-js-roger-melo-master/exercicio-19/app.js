24e95c6a148111296c151dbf6e7253c41acb7c2e
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-26 17:55:24 -0300

    finishing the quiz

--- curso-de-js-roger-melo-master/exercicio-19/app.js
+++ curso-de-js-roger-melo-master/exercicio-19/app.js
@@ -12,9 +12,37 @@ Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, bus
 É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
 */
 
-const button = document.querySelector('.btn')
-const questionsContainer = document.querySelector('.questions-container')
-button.addEventListener('click', () => {
-    questionsContainer.style.display = "inherit"
+const button = document.querySelector(".btn");
+const send = document.querySelector(".send");
+const questionsContainer = document.querySelector(".questions-container");
+const questionOne = document.querySelector(".form-group_um");
+const questionTwo = document.querySelector(".form-group_dois");
+const questionThree = document.querySelector(".form-group_tres");
+let counter = 0;
+button.addEventListener("click", () => {
+    if (counter == 0)
+	questionsContainer.style.display = "inherit";
+    questionOne.style.display = "inherit";
+});
 
-})
+send.addEventListener("click", () => {
+	counter = counter + 1;
+	if (counter == 1) {
+        console.log(counter, 'counter 1')
+		questionTwo.style.display = "inherit";
+		questionOne.style.display = "none";
+	}
+    if (counter == 2) {
+        console.log(counter, 'counter 2')
+        questionThree.style.display = 'inherit'
+        questionTwo.style.display = "none";
+        questionOne.style.display = "none";
+    }
+    if (counter == 3) {
+        console.log(counter, 'counter 3')
+        counter = 0
+        questionThree.style.display = 'none'
+        questionsContainer.style.display = "none";
+        console.log(counter)
+    }
+});
