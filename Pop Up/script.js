292bccc2ddb0abd1a8b776239c544ae50758a9fa
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-26 13:36:40 -0300

    popup

--- Pop Up/script.js
+++ Pop Up/script.js
@@ -1,16 +1,14 @@
 // a exibição do popup depende de um click no botão
 const button = document.querySelector('button')
 const popup = document.querySelector('.popup-wrapper')
-const close = document.querySelector('.popup-close')
 
 button.addEventListener('click', () => {
     popup.style.display = "block"
 })
 
-close.addEventListener('click', () => { 
-    popup.style.display = 'none'
+
+popup.addEventListener('click', (e) => {
+    console.log(e.target)
+    //popup.style.display = 'none'
 })
 
-popup.addEventListener('click', () => {
-    popup.style.display = 'none'
-})
