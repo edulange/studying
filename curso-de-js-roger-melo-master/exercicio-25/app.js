67d94f37c346eb5ae1a02f932921122ac1993819
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-01 16:17:11 -0300

    26

--- curso-de-js-roger-melo-master/exercicio-25/app.js
+++ curso-de-js-roger-melo-master/exercicio-25/app.js
@@ -20,6 +20,17 @@ const months = [
   'Dezembro'
 ]
 
+const indexOfFeb = months.findIndex(month => {
+  return month === 'Fevereiro'
+})
+console.log(indexOfFeb) //jeito 1
+
+const elementIndex = months.findIndex(CheckMonth);
+function CheckMonth(element, index, array) {
+  return index
+}
+console.log(elementIndex) //jeito 2
+
 /*
   02
 
@@ -27,24 +38,60 @@ const months = [
   - Exiba o objeto no console.
 */
 
+let dataAtual = new Date()
+const presentMoment = {
+  day: dataAtual.getDate(),
+  month: dataAtual.getMonth(),
+  year: dataAtual.getFullYear()
+}
+console.log(dataAtual, 'Data Atual')
+console.log(presentMoment)
+
+
+
+
+console.log("=========================================")
+
+const formatDateInfo = date => {
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  return `${hours}:${minutes} - Segunda, 29 de Outubro de 2022`
+}
+const date = new Date()
+
+console.log(formatDateInfo(date))
+console.log("=========================================")
+
+
 /*
   03
 
   - Baseado no objeto que você acabou de criar, exiba o ano atual no console.
 */
-
+console.log(presentMoment.year, 'ano')
 /*
-  04
+04
 
-  - Crie um objeto de data que represente um momento passado;
-  - Exiba o objeto no console.
+- Crie um objeto de data que represente um momento passado;
+- Exiba o objeto no console.
 */
+
+const pastDate = {
+  day: 05,
+  month: 02,
+  year: 1994
+}
+
+
+
 
 /*
   05
 
   - Exiba, no console, a hora do objeto que você acabou de criar.
 */
+
+console.log(pastDate)
 
 /*
   06
@@ -53,11 +100,19 @@ const months = [
   - Exiba o objeto no console.
 */
 
+const futureDate = {
+  day: 25,
+  month: 2,
+  year: 2030
+}
+
 /*
   07
 
   - Exiba no console a quantidade de dias entre o momento futuro e o passado.
 */
+
+
 
 /*
   08
