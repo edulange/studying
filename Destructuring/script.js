9392f54f2e7d5adeada809276b11be82552cad5d
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-12-08 20:26:18 -0300

    destructuringggg

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -12,40 +12,68 @@ console.log("breed", breed);
 
 // outro exemplo
 const cities = {
-    paris: {
-        country: 'France',
-        language: 'French'
-    },
-    stockholm: {
-        country: 'Sweden',
-        language: 'Swedish'
-    }
-}
+	paris: {
+		country: "France",
+		language: "French",
+	},
+	stockholm: {
+		country: "Sweden",
+		language: "Swedish",
+	},
+};
 
-const { paris, stockholm: { country } } = cities
+const {
+	paris,
+	stockholm: { country },
+} = cities;
 
-console.log('paris', paris)
-console.log('country', country)
+console.log("paris", paris);
+console.log("country", country);
 
 //const getName = options => options.name //mesma coisa q em baixo, mas muito mais legível
-const getName = ({ name }) => name
-console.log(getName({name: 'Marcelo'}))
+const getName = ({ name }) => name;
+console.log(getName({ name: "Marcelo" }));
 
-const fruits = ['banana', 'pera', 'uva']
+const fruits = ["banana", "pera", "uva"];
 
-let [a, ...rest] = fruits
+let [a, ...rest] = fruits;
 
-console.log('a, :>> ', a);
-console.log('rest :>> ', rest);
-console.log('fruits :>> ', fruits);
+console.log("a, :>> ", a);
+console.log("rest :>> ", rest);
+console.log("fruits :>> ", fruits);
 
 function liquidificador(xd) {
-    console.log('frutas :>> ', xd);
+	console.log("frutas :>> ", xd);
 }
 
 const comidas = {
-    banana: 'banana',
-    pera: 'pera'
-}
+	banana: "banana",
+	pera: "pera",
+};
 
-liquidificador(comidas.pera)
+liquidificador(comidas.pera);
+//--------------------------===============
+const numerosPares = [2, 4, 6];
+const numerosImpares = [1, 3, 5];
+
+const numSemDestruc = [numerosPares, numerosImpares];
+console.log("numeros s/ destructu", numSemDestruc);
+const numComDestruc = [...numerosPares, ...numerosImpares];
+console.log("numeros c/ destructuring", numComDestruc);
+
+/*
+const num1 = 1
+const num2 = 2
+são iguais
+const [num1, num2] = [1, 2]
+*/
+const [num1, num2, ...outrosNum] = [1, 2, 3, 4, 5, 6]; //é a mesma coisa q em cima
+console.log('num1', num1)
+console.log('num2', num2)
+console.log('OutrosNum', outrosNum)
+
+
+//const [nome1 = 'Eduardo'] = [1]
+// aqui se eu der um console.log(nome1) ele vai printar nome1 = 1
+const [nome1 = 'Eduardo'] = []
+console.log('nome1', nome1) //já aqui, ele printa Eduardo, pq ele seria undefined
